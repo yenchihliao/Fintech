81390d58b58775325e8e5aa18df6df43b2f92201
yenchihliao <yen.yenchih.liao@gmail.com>
2019-09-28 21:41:50 +0800

    submit version

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,27 +1,63 @@
 import os
 import sys
-import pandas as pd
 import numpy as np
-filename = sys.argv[1]
-df = pd.read_csv(filename,encoding='big5',dtype={'成交日期' : str,
-                                                 '商品代號' : str,
-                                                 '到期月份(週別)' : str,
-                                                 '成交時間' : int,
-                                                 '成交價格' : float})
-df['商品代號']=df['商品代號'].map(str.strip)
-df['到期月份(週別)']=df['到期月份(週別)'].map(str.strip)
-df = df[df['商品代號']=='TX']
-df = df[df['成交時間']>=84500]
-df = df[df['成交時間']<=134500]
-df = df[df['到期月份(週別)'].map(len)==6]
-mintime = (df['到期月份(週別)'].astype(np.int32).min())
-df = df[df['到期月份(週別)'].astype(np.int32)==mintime]
-try:
-    op = df.loc[df['成交時間'].idxmin()]
-    high = df.loc[df['成交價格'].idxmax()]
-    low = df.loc[df['成交價格'].idxmin()]
-    close = df[df['成交時間'] == df['成交時間'].max()]
-    close = close.iloc[-1]
-    print('{} {} {} {}'.format(int(op[4]), int(high[4]), int(low[4]), int(close[4])))
-except:
-    print('0 0 0 0')
+
+class Tx:
+    def __init__(self, args):#deal_date, product, due_month, deal_time, deal_price):
+        self._deal_date = args[0].strip()#deal_date
+        self._product = args[1].strip()#product
+        self._due_month = args[2].strip()#due_month
+        self._deal_time = int(args[3].strip())#deal_time
+        self._deal_price = float(args[4].strip())#deal_price
+    def show(self):
+        print(self._deal_date, self._product, self._due_month, self._deal_time, self._deal_price)
+
+class LegalTx():
+    def __init__(self):
+        self._list = []
+        self._min_month = 10000000
+    def tryAdd(self, tx):
+        if tx._product != 'TX':
+            return
+        if tx._deal_time < 84500:
+            return
+        if tx._deal_time > 134500:
+            return
+        if len(tx._due_month) != 6:
+            return
+        if int(tx._due_month) > int(self._min_month):
+            return
+        if int(tx._due_month) < int(self._min_month):
+            self._min_month = tx._due_month
+            self._list = []
+        self._list.append(tx)
+    def show(self):
+        for e in self._list:
+            e.show()
+
+f = open(sys.argv[1], 'r', encoding='big5')
+
+count = 0
+txList = LegalTx()
+f.readline()
+while True:
+    line = f.readline()
+    if not line:
+        break
+    txList.tryAdd(Tx(line.split(',')))
+    count += 1
+
+if len(txList._list) == 0:
+    print("0 0 0 0")
+else:
+    first = txList._list[0]._deal_price
+    last = txList._list[len(txList._list) - 1]._deal_price
+    pMax = first
+    for tx in txList._list:
+        if tx._deal_price > pMax:
+            pMax = tx._deal_price
+    pMin = first
+    for tx in txList._list:
+        if tx._deal_price < pMin:
+            pMin = tx._deal_price
+    print(int(first), int(pMax), int(pMin), int(last))
